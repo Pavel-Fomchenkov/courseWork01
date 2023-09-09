cf529c63145b2822b9d8b3853b8199323c37fbcf
Pavel-Fomchenkov <donotwritemail@gmail.com>
2023-09-10 00:11:07 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,32 @@ public class Main {
         }
     }
 
-    public static void getAllEmployees() {
+    public static void printEmployees() {
         for (Employee employee : employees) {
-            if (employee != null) System.out.println(employee);
+            if (employee == null) {
+                break;
+            } else System.out.println(employee);
+        }
+    }
+
+    public static void printEmployees(int department) {
+        boolean hasEmployee = false;
+        for (Employee employee : employees) {
+            if (employee == null) {
+                break;
+            } else if (employee.getDepartment() == department) {
+                hasEmployee = true;
+                System.out.printf("id: %d - %s %s %s, оклад: %.2f\n",
+                        employee.getId(), employee.getLastName(), employee.getFirstName(), employee.getMiddleName(), employee.getSalary());
+            }
+        }
+        if (!hasEmployee) System.out.printf("Работники из отдела %d не найдены.\n", department);
+    }
+
+    public static void printEmployees(Employee... employees) {
+        if (employees.length == 0) System.out.println("нет данных");
+        if (employees.length > 0) {
+            for (Employee e : employees) System.out.println(e);
         }
     }
 
@@ -28,56 +51,104 @@ public class Main {
         return expences;
     }
 
+    public static double expensesSalary(int department) {
+        double expences = 0.0;
+        for (Employee employee : employees) {
+            if (employee == null) {
+                break;
+            } else if (employee.getDepartment() == department) {
+                expences += employee.getSalary();
+            }
+        }
+        return expences;
+    }
+
+
     public static Employee minSalaryEmployee() {
-        double minSalary = 0.0;
+        double minSalary = employees[0].getSalary();
         int index = 0;
-        int k = 1;
-        // looking for first nonempty cell in employees
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] != null) {
+        int k = 0;
+        for (int i = 1; i < employees.length; i++) {
+            if (employees[i] == null) {
+                break;
+            } else if (minSalary > employees[i].getSalary()) {
                 minSalary = employees[i].getSalary();
                 index = i;
-                break;
             }
         }
-        // looking for index of desired cell in employees
-        for (int j = 1; j < employees.length; j++) {
-            if (employees[j] != null && minSalary >= employees[j].getSalary()) {
-                if (minSalary == employees[j].getSalary()) k++;
-                minSalary = employees[j].getSalary();
-                index = j;
-            }
+        for (Employee employee : employees) {
+            if (employee == null) break;
+            if (employee.getSalary() == minSalary) k++;
         }
-        // alert message
         if (k > 1) {
             System.out.printf("ВНИМАНИЕ! В базе данных обнаружено %d сотрудников с минимальной зарплатой %.2f руб. \n", k, employees[index].getSalary());
         }
         return employees[index];
     }
 
-    public static Employee maxSalaryEmployee() {
-        double maxSalary = 0.0;
-        int index = 0;
-        int k = 1;
-        // looking for first nonempty cell in employees
+    public static Employee minSalaryEmployee(int department) {
+        double minSalary = 0.0;
+        int index = 1;
+        int k = 0;
         for (int i = 0; i < employees.length; i++) {
-            if (employees[i] != null) {
+            if (employees[i] == null) {
+                break;
+            } else if (employees[i].getDepartment() == department && (minSalary > employees[i].getSalary() || minSalary == 0.0)) {
+                minSalary = employees[i].getSalary();
+                index = i;
+            }
+        }
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) break;
+            if (employees[i].getDepartment() == department && employees[i].getSalary() == minSalary) k++;
+        }
+        if (k > 1) {
+            System.out.printf("ВНИМАНИЕ! В отделе %d обнаружено %d сотрудников с минимальной зарплатой %.2f руб. \n", department, k, employees[index].getSalary());
+        }
+        return employees[index];
+    }
+
+
+    public static Employee maxSalaryEmployee() {
+        double maxSalary = employees[0].getSalary();
+        int index = 0;
+        int k = 0;
+        for (int i = 1; i < employees.length; i++) {
+            if (employees[i] == null) {
+                break;
+            } else if (maxSalary < employees[i].getSalary()) {
                 maxSalary = employees[i].getSalary();
                 index = i;
-                break;
             }
         }
-        // looking for index of desired cell in employees
-        for (int j = 1; j < employees.length; j++) {
-            if (employees[j] != null && maxSalary <= employees[j].getSalary()) {
-                if (maxSalary == employees[j].getSalary()) k++;
-                maxSalary = employees[j].getSalary();
-                index = j;
-            }
+        for (Employee employee : employees) {
+            if (employee == null) break;
+            if (employee.getSalary() == maxSalary) k++;
         }
-        // alert message
         if (k > 1) {
             System.out.printf("ВНИМАНИЕ! В базе данных обнаружено %d сотрудников с максимальной зарплатой %.2f руб. \n", k, employees[index].getSalary());
+        }
+        return employees[index];
+    }
+
+    public static Employee maxSalaryEmployee(int department) {
+        double maxSalary = 0.0;
+        int index = 0;
+        int k = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                break;
+            } else if (employees[i].getDepartment() == department && maxSalary < employees[i].getSalary()) {
+                maxSalary = employees[i].getSalary();
+                index = i;
+            }
+        }
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) break;
+            if (employees[i].getDepartment() == department && employees[i].getSalary() == maxSalary) k++;
+        }
+        if (k > 1) {
+            System.out.printf("ВНИМАНИЕ! В отделе %d обнаружено %d сотрудников с максимальной зарплатой %.2f руб. \n", department, k, employees[index].getSalary());
         }
         return employees[index];
     }
@@ -89,31 +160,120 @@ public class Main {
                 count++;
             }
         }
-        return (double) Math.round(expensesSalary() / count * 100) / 100;
+        return Math.round(expensesSalary() / count * 100) / 100.0;
     }
+
+    public static double averageSalary(int department) {
+        int count = 0;
+        for (Employee employee : employees) {
+            if (employee == null) {
+                break;
+            } else if (employee.getDepartment() == department) {
+                count++;
+            }
+        }
+        return Math.round(expensesSalary(department) / count * 100) / 100.0;
+    }
+
     public static void printNames() {
         boolean comma = false;
         for (Employee employee : employees) {
             if (employee != null) {
-                if(comma) System.out.print(", ");
+                if (comma) System.out.print(", ");
                 System.out.printf("%s %s %s", employee.getLastName(), employee.getFirstName(), employee.getMiddleName());
                 comma = true;
             }
         }
     }
 
-    public static void main(String[] args) {
-        addEmployee("Иванова","Елена", "Петровна",  1, 1000.0);
-        addEmployee("Молодец","Геннадий", "Иванович",  2, 1200.33);
-        addEmployee("Муромова","Василиса", "Ильинична",  1, 1000.0);
-        addEmployee("Иванов","Иван", "Иванович",  2, 1800.33);
-        addEmployee("Воробьев","Евгений", "Карпович",  2, 1800.33);
+    public static void increaseSalary(int percent) {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                employee.setSalary(Math.round(employee.getSalary() * (100 + percent)) / 100.0);
+            }
+        }
+    }
 
-        getAllEmployees();
+    public static void increaseSalary(int percent, int department) {
+        for (Employee employee : employees) {
+            if (employee == null) {
+                break;
+            } else if (employee.getDepartment() == department) {
+                employee.setSalary(Math.round(employee.getSalary() * (100 + percent)) / 100.0);
+            }
+        }
+    }
+
+    public static Employee[] employeeWithSalaryLowerOrEqualThen(double salary) {
+        int[] indices = new int[employees.length];
+        int indicesLength = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                break;
+            } else if (employees[i].getSalary() <= salary) {
+                indices[indicesLength] = i;
+                indicesLength++;
+            }
+        }
+        Employee[] selected = new Employee[indicesLength];
+        if (indicesLength == 0) return selected;
+        boolean flag = true;
+        for (int i = 0; i < indices.length; i++) {
+            if (indices.length == 0 || indices[i] == 0 && !flag) break;
+            selected[i] = employees[indices[i]];
+            flag = false;
+        }
+        return selected;
+    }
+
+    public static Employee[] employeeWithSalaryHigherOrEqualThen(double salary) {
+        int[] indices = new int[employees.length];
+        int indicesLength = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                break;
+            } else if (employees[i].getSalary() >= salary) {
+                indices[indicesLength] = i;
+                indicesLength++;
+            }
+        }
+        Employee[] selected = new Employee[indicesLength];
+        if (indicesLength == 0) return selected;
+        boolean flag = true;
+        for (int i = 0; i < indices.length; i++) {
+            if (indices.length == 0 || indices[i] == 0 && !flag) break;
+            selected[i] = employees[indices[i]];
+            flag = false;
+        }
+        return selected;
+    }
+
+    public static void main(String[] args) {
+        addEmployee("Иванова", "Елена", "Петровна", 1, 1050.0);
+        addEmployee("Молодец", "Геннадий", "Иванович", 2, 1100.0);
+        addEmployee("Муромова", "Василиса", "Ильинична", 1, 1000.0);
+        addEmployee("Иванов", "Иван", "Иванович", 3, 1850.0);
+        addEmployee("Воробьев", "Евгений", "Карпович", 2, 1200.0);
+        addEmployee("Толстой", "Лев", "Николаевич", 4, 1800.0);
+        addEmployee("Весёлый", "Алексей", "Алексеевич", 5, 1400.0);
+        addEmployee("Петров", "Пётр", "Петрович", 2, 1400.0);
+
+        printEmployees();
         System.out.printf("Расходы на оплату труда составят %.2f руб.\n", expensesSalary());
         System.out.printf("Сотрудник с минимальной зарплатой - %s.\n", minSalaryEmployee());
         System.out.printf("Сотрудник с максимальной зарплатой - %s.\n", maxSalaryEmployee());
         System.out.printf("Средняя зарплата: %.2f руб.\n", averageSalary());
         printNames();
+        System.out.println(" ");
+        //   increaseSalary(10);
+        //printEmployees();
+        //   System.out.printf("Сотрудник с минимальной зарплатой в отделе 2 - %s.\n", minSalaryEmployee(2));
+        //  System.out.printf("Сотрудник с максимальной зарплатой в отделе 2 - %s.\n", maxSalaryEmployee(2));
+        //System.out.println(expensesSalary(2));
+        //System.out.println(averageSalary(2));
+        // increaseSalary(10, 2);
+        //printEmployees(2);
+        printEmployees(employeeWithSalaryLowerOrEqualThen(1000.0));
+        printEmployees(employeeWithSalaryHigherOrEqualThen(1850.0));
     }
 }
